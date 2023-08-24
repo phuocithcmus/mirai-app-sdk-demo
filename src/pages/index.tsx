e506d7c4790133f2355678870046637fc4300ef6
phuocnd <ducphuoc.t9@gmail.com>
2023-08-24 16:37:23 +0700

    chore: new event

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -28,6 +28,7 @@ export default function Home() {
   //   useState<MiraiConnection | null>(null);
   const [showSignArea, setShowSignArea] = useState<boolean>(false);
   const [isConnectting, setIsConnectting] = useState<boolean>(false);
+  const [isGettting, setIsGetting] = useState<boolean>(false);
   const [qrcode, setQrCode] = useState<string>("");
   const [uri, setUri] = useState<string>("");
   const [socketId, setSocketId] = useState<string>("");
@@ -70,10 +71,12 @@ export default function Home() {
     setUri("");
     setTopicId("");
     setMessage("");
+    setIsConnectting(false);
   });
 
   socket.on("disconnect", (reason) => {
     console.log("disconnected", reason);
+    setIsConnectting(false);
     setMessage(`ws disconnected: reason: ${reason}`);
   });
 
@@ -96,6 +99,8 @@ export default function Home() {
   socket.on("error", (message) => {
     console.log("error", message);
     setMessage(message);
+
+    setIsConnectting(false);
   });
 
   socket.on("response", (response) => {
@@ -183,14 +188,16 @@ export default function Home() {
   // }, [status]);
 
   const getTopic = async () => {
-    const topic = await axios.get(
-      "https://dev-sign-provider.miraiid.io/api/provider",
-      {
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
-        },
-      }
-    );
+    try {
+      const topic = await axios.get(
+        "https://dev-sign-provider.miraiid.io/api/provider",
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        }
+      );
+    } catch (e) {}
   };
 
   return (
@@ -242,33 +249,32 @@ export default function Home() {
       <div className="relative flex place-items-center ">
         <>
           <button
-            disabled={isConnectting}
             data-modal-target="defaultModal"
             data-modal-toggle="defaultModal"
             className="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             type="button"
             onClick={async () => {
+              setIsConnectting(true);
               socket.connect();
             }}
           >
-            Connect
+            {isConnectting ? "Connectting" : "Connect"}
           </button>
           <button
-            disabled={isConnectting}
             data-modal-target="defaultModal"
             data-modal-toggle="defaultModal"
             className="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             type="button"
             style={{ marginLeft: "10px" }}
             onClick={async () => {
-              setIsConnectting(true);
+              setIsGetting(true);
 
               await getTopic();
 
-              setIsConnectting(false);
+              setIsGetting(false);
             }}
           >
-            Get QRCode
+            {isGettting ? "watting..." : "Get QRCode"}
           </button>
         </>
         {/* {isConnectting ||
