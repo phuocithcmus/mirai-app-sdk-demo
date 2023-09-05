7ba7dba9a6d71564a541d06a330237e9bc4c2c79
phuocnd <ducphuoc.t9@gmail.com>
2023-09-05 16:41:53 +0700

    chore: update noti account/chain changed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -417,6 +417,7 @@ export default function Home() {
         </p>
       </div>
       <input
+        value={accessToken}
         type="text"
         className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
         placeholder="Access Token"
