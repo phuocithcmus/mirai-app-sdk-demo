862e6c4d50825d74b0c3bb0971797f1b698cde7c
phuocnd <ducphuoc.t9@gmail.com>
2023-09-21 16:45:27 +0700

    chore: update modal

--- src/app-components/CustomWindow/CustomWindow.ts
+++ src/app-components/CustomWindow/CustomWindow.ts
@@ -1,9 +1,13 @@
 import { AbstractMiraiWindow } from "@mirailabs-co/miraiid-js";
 
+export const SIGN_BASE_URL = "http://mirai-app-sdk-demo.vercel.app";
+
 class CustomWindow extends AbstractMiraiWindow {
   open({ uri }: { uri: string }): Promise<void> {
     return new Promise((resolve, reject) => {
-      window.open(uri);
+      window.open(
+        `http://id-web-local.mirailabs.co/sign?w=${encodeURIComponent(uri)}`
+      );
     });
   }
   close(): Promise<void> {
