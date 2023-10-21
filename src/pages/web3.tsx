1d2fcedb5625eb312a8ece6171078de9a2c39899
phuocnd <ducphuoc.t9@gmail.com>
2023-10-21 11:09:46 +0700

    chore: update wc

--- src/pages/web3.tsx
+++ src/pages/web3.tsx
@@ -6,12 +6,14 @@ import {
 import { Connector } from "@web3-react/types";
 import { WalletConnect as WalletConnectV2 } from "@web3-react/walletconnect-v2";
 import wcConnector, { MAINNET_CHAINS } from "../connectors/wc-connector";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import {
   useAuthentication,
   WalletName,
   useActiveWeb3React,
+  isMobile,
 } from "@pegaxy/auth-sdk";
+import toast from "react-hot-toast";
 
 // const [walletConnectV2, walletConnectV2Hooks] = wcConnector();
 
@@ -31,32 +33,43 @@ export function getName(connector: Connector) {
 //   [walletConnectV2, walletConnectV2Hooks],
 // ];
 
-function Child({ wallet }: any) {
+function Child({ wallet, auth }: any) {
   console.log(wallet);
 
   const { account, isActive } = useActiveWeb3React();
+  const [loading, setLoading] = useState<boolean>(false);
+
+  // useEffect(() => {
+  //   (async () => {
+  //     if (wallet) {
+  //       const [connector, _] = wallet;
+  //       await connector.activate();
+
+  //       console.log(`Priority Connector is:connector}`, getName(connector));
+  //     }
+  //   })();
+  // }, [wallet]);
 
   useEffect(() => {
     (async () => {
-      if (wallet) {
-        const [connector, _] = wallet;
-        await connector.activate();
-
-        console.log(`Priority Connector is:connector}`, getName(connector));
+      if (auth) {
+        setLoading(true);
+        await auth.connect(WalletName.WalletConnect);
+        setLoading(false);
       }
     })();
-  }, [wallet]);
+  }, [auth]);
 
-  useEffect(() => {
-    (async () => {
-      const [connector, _] = wallet;
-      if (!isActive) {
-        await connector.connectEagerly();
-      } else {
-        console.log(account);
-      }
-    })();
-  }, [account, isActive]);
+  // useEffect(() => {
+  //   (async () => {
+  //     const [connector, _] = wallet;
+  //     if (!isActive) {
+  //       await connector.connectEagerly();
+  //     } else {
+  //       console.log(account);
+  //     }
+  //   })();
+  // }, [account, isActive]);
 
   return (
     <button
@@ -66,30 +79,37 @@ function Child({ wallet }: any) {
           await connector.deactivate();
 
           console.log(`Priority Connector is:connector}`, getName(connector));
+          toast("Disconnected");
         }
       }}
     >
-      Disconnect
+      {isActive && "Disconnect"}
+      {!isActive && loading && "Loading"}
     </button>
   );
 }
 
 export default function ProviderExample() {
   const auth_client = useAuthentication(
-    80001,
+    2195,
     {
       appName: "Pegaxy",
       appLogo:
         "https://cdn.pegaxy.io/statics/play/public/v11/images/pegaxy.png",
-      allowedWallets: SUPPORTED_WALLETS,
-      allowedchains: MAINNET_CHAINS,
+      allowedWallets: !(() => isMobile())
+        ? SUPPORTED_WALLETS
+        : SUPPORTED_WALLETS.concat(WalletName.StandaloneEIP1193),
+      // allowedchains: MAINNET_CHAINS,
     },
     [2195]
   );
 
   return (
     <Web3ReactProvider connectors={auth_client.auth.connectors}>
-      <Child wallet={auth_client.auth.getConnector(WalletName.WalletConnect)} />
+      <Child
+        wallet={auth_client.auth.getConnector(WalletName.WalletConnect)}
+        auth={auth_client.auth}
+      />
     </Web3ReactProvider>
   );
 }
