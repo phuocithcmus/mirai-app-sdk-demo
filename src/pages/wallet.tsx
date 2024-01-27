0c310e4a6dced60013dc9af7d3f5b0b907e63779
phuocnd <ducphuoc.t9@gmail.com>
2024-01-28 00:59:41 +0700

    chore: update

--- src/pages/wallet.tsx
+++ src/pages/wallet.tsx
@@ -1,9 +1,11 @@
 import { Button } from "@mui/material";
 import {
+  ConnectWallet,
   SignerOrProvider,
-  ThirdwebProvider,
   metamaskWallet,
+  useAddress,
   useConnect,
+  useSigner,
 } from "@thirdweb-dev/react";
 import { getInjectedMetamaskProvider } from "@thirdweb-dev/wallets";
 import { useEffect, useState } from "react";
@@ -12,8 +14,12 @@ const metamask = metamaskWallet();
 
 const WalletPage = () => {
   const connect = useConnect();
-  const [signer, setSigner] = useState<SignerOrProvider | null>(null);
-  const [address, setAddress] = useState<string | null>(null);
+  //   const [signer, setSigner] = useState<SignerOrProvider | null>(null);
+  //   const [address, setAddress] = useState<string | null>(null);
+
+  const signer = useSigner();
+  const address = useAddress();
+  const [connecting, setConnecting] = useState<boolean>(false);
 
   useEffect(() => {
     if (!metamask || !getInjectedMetamaskProvider()) {
@@ -26,23 +32,42 @@ const WalletPage = () => {
       <>
         <Button
           onClick={async () => {
+            setConnecting(true);
             const wallet = await connect(metamask);
 
-            const signer = await wallet.getSigner();
-            setSigner(signer);
-            setAddress(await signer.getAddress());
+            // const signer = await wallet.getSigner();
+            // setSigner(signer);
+            // setAddress(await signer.getAddress());
+            setConnecting(false);
           }}
         >
-          Connect Metamask
+          Connect Metamask with hook
         </Button>
-
-        {signer && address && (
-          <div>
-            <p>Connected to Metamask</p>
-            <p>{address}</p>
-          </div>
-        )}
       </>
+
+      <ConnectWallet
+        theme="dark"
+        modalSize="compact"
+        className="btn flex-grow-1"
+        btnTitle={
+          (
+            <>
+              {connecting ? (
+                <span>connecting...</span>
+              ) : (
+                <span>Connect Wallet default Thirdweb</span>
+              )}
+            </>
+          ) as any
+        }
+      />
+
+      {signer && address && (
+        <div>
+          <p>Connected to Metamask</p>
+          <p>{address}</p>
+        </div>
+      )}
     </>
   );
 };
