56b6fd28727a7db4f2673e027684761644e5a2e5
phuocnd <ducphuoc.t9@gmail.com>
2024-02-05 13:48:09 +0700

    chore: off children for test

--- src/context/FeatureToggleContext.tsx
+++ src/context/FeatureToggleContext.tsx
@@ -1,45 +1,59 @@
-import { useOnEventCallback } from '@app-helpers';
-import _ from 'lodash';
-import { createContext, ReactNode, useContext, useEffect, useState } from 'react';
+import _ from "lodash";
+import {
+  createContext,
+  ReactNode,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
 
 // Add feature name here
 export enum Feature {
-	App = 'application',
-	DataTracking = 'data_tracking',
-	KeyAndLootbox = 'key_n_lootbox',
+  App = "application",
+  DataTracking = "data_tracking",
+  KeyAndLootbox = "key_n_lootbox",
 }
 
 interface FeatureToggleProviderProps {
-	children: ReactNode;
+  children: ReactNode;
 }
 
 interface FeatureToggleContextDefault {
-	isEnabled?: (feature_name: Feature) => boolean;
+  isEnabled?: (feature_name: Feature) => boolean;
 }
 
 const ftDefault = {};
 
-const FeatureToggleContext = createContext<FeatureToggleContextDefault>(ftDefault);
+const FeatureToggleContext =
+  createContext<FeatureToggleContextDefault>(ftDefault);
 
-const FeatureToggleContextProvider = ({ children }: FeatureToggleProviderProps) => {
-	const [features, setFeatures] = useState<string[]>([]);
+const FeatureToggleContextProvider = ({
+  children,
+}: FeatureToggleProviderProps) => {
+  const [features, setFeatures] = useState<string[]>([]);
 
-	const isEnabled = useOnEventCallback((feature_name: Feature) => {
-		return _.includes(features, feature_name.toString());
-	});
+  const isEnabled = (feature_name: Feature) => {
+    return _.includes(features, feature_name.toString());
+  };
 
-	useEffect(() => {
-		const str_raw_features = (process.env.NEXT_PUBLIC_ENABLED_FEATURES ?? '') as string;
-		setFeatures(_.split(str_raw_features, ',').map((f) => f.trim()));
-	}, []);
+  useEffect(() => {
+    const str_raw_features = (process.env.NEXT_PUBLIC_ENABLED_FEATURES ??
+      "") as string;
+    setFeatures(_.split(str_raw_features, ",").map((f) => f.trim()));
+  }, []);
 
-	return <FeatureToggleContext.Provider value={{ isEnabled }}>{children}</FeatureToggleContext.Provider>;
+  return (
+    <FeatureToggleContext.Provider value={{ isEnabled }}>
+      {children}
+    </FeatureToggleContext.Provider>
+  );
 };
 
 export const isFeatureEnabled = (feature: Feature) => {
-	const str_raw_features = (process.env.NEXT_PUBLIC_ENABLED_FEATURES ?? '') as string;
-	const features = _.split(str_raw_features, ',').map((f) => f.trim());
-	return _.includes(features, feature.toString());
+  const str_raw_features = (process.env.NEXT_PUBLIC_ENABLED_FEATURES ??
+    "") as string;
+  const features = _.split(str_raw_features, ",").map((f) => f.trim());
+  return _.includes(features, feature.toString());
 };
 
 export const useFeatureToggles = () => useContext(FeatureToggleContext);
